46c54d9ea38595c3a664f4a9fd5158101eb22fe5
Valter Vianês <valter.vianes@gmail.com>
2022-09-16 21:58:38 +0200

    Add project files.

---
+++ Bucket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SyntraWestAdvancedDotNetEventsExercise4
+{
+    class Bucket
+    {
+        public int _maxAmount { get; set; }
+        private int _currentAmount { get; set; }
+        public event EventHandler AlmostFull;
+        public event EventHandler Full;
+
+        public Bucket(int maxAmount)
+        {
+            _maxAmount = maxAmount;
+        }
+
+        public void Fill(int _amount)
+        {
+            _currentAmount += _amount;
+
+            if (_currentAmount < 0.8 * _maxAmount)
+            {
+                _currentAmount += _amount;
+            }
+            else if (_currentAmount >= 0.8 * _maxAmount && _currentAmount < _maxAmount)
+            {
+                AlmostFull.Invoke(this, null);
+            }
+            else
+            {
+                Full.Invoke(this, null);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SyntraWestAdvancedDotNetEventsExercise4
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bucket = new Bucket(100);
+
+            bucket.AlmostFull += Bucket_AlmostFull;
+            bucket.Full += Bucket_Full;
+
+            bucket.Fill(85);
+        }
+
+        private static void Bucket_AlmostFull(object sender, EventArgs e)
+        {
+            Console.WriteLine("The bucket is almost full");
+        }
+
+        private static void Bucket_Full(object sender, EventArgs e)
+        {
+            Console.WriteLine("The bucket is full");
+        }
+
+   
+    }
+}
